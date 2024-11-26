62c36a472ebd6dd7038f00eeb22ffd3c18fe4881
musicmalgon <musicmalgon@gmail.com>
2024-11-26 15:10:07 +0900

    fix: 멤버 수정 및 탈퇴 에러 수정
    - password matches 에러 수정
    - member id 못찾는 에러 수정
    
    Resolves: #14

--- src/main/java/com/sparta/concurrencycontrolproject/domain/ticket/controller/TicketController.java
+++ src/main/java/com/sparta/concurrencycontrolproject/domain/ticket/controller/TicketController.java
@@ -19,6 +19,12 @@ import com.sparta.concurrencycontrolproject.domain.ticket.service.TicketService;
 import com.sparta.concurrencycontrolproject.security.UserDetailsImpl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
