1779efd4162ee175ec76b9a3058375e54fc6710f
Minwoo <alsdn2606@gmail.com>
2024-11-27 15:34:13 +0900

    fix: 버그 수정
    - 권한이 0, 1 로 기입되던 버그 수정
    - JpaAuditing 기능 추가 및 시간 기능 활성화

--- src/main/java/com/sparta/concurrencycontrolproject/ConcurrencyControlProjectApplication.java
+++ src/main/java/com/sparta/concurrencycontrolproject/ConcurrencyControlProjectApplication.java
@@ -2,12 +2,14 @@ package com.sparta.concurrencycontrolproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+@EnableJpaAuditing
 public class ConcurrencyControlProjectApplication {
 
     public static void main(String[] args) {
